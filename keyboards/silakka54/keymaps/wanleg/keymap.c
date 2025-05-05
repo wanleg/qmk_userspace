3750b493b3fd7a8239274e6b6809df3b79ca4215
box <box@localhost.localdomain>
2025-05-05 15:57:19 -0700

    working silakka54 keymap

--- keyboards/silakka54/keymaps/wanleg/keymap.c
+++ keyboards/silakka54/keymaps/wanleg/keymap.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 #include "wanleg.h"
 
+//Copy of Iris layout minus the two extra keys
 //Combo section start
 //list combos
 enum combo_events {
@@ -38,23 +39,6 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
 }
 //Combo section end
 
-/* no numrow layout
-#define LAYOUT_iris_base( \
-    K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, \
-    K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, \
-    K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, K2C, \
-    K31, K32, K33, K34, K35, K36, K37, K38, K39, K3A, K3B, K3C  \
-  ) \
-  LAYOUT_wrapper( \
-     K01,	K02,	K03,	K04,	K05,	K06,						K07,	K08,	K09,	K0A,	K0B,	K0C, \
-     K11,	K12,	K13,	K14,	K15,	K16,						K17,	K18,	K19,	K1A,	K1B,	K1C, \
-     K21,	K22,	K23,	K24,	K25,	K26,						K27,	K28,	K29,	K2A,	K2B,	K2C, \
-     K31,	K32,	K33,	K34,	K35,	K36,	DIRECTION, NUMBER,			K37,	K38,	K39,	K3A,	K3B,	K3C, \
-                               _______, _______, SH_MON,			SH_MON, _______, _______                  \
-  )
-*/
-
-
 #define LAYOUT_iris_base( \
     K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, K0C, \
     K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, K1C, \
@@ -78,38 +62,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______________GherkinLike_1_______________,
     _______________GherkinLike_2_______________
   ),
-/*  
-  [ONE] = LAYOUT_wrapper(
-    _______________SUBTER_Row__1_______________,
-    _______________Qwerty_Row__0_______________,
-    _______________Qwerty_Row__1_______________,
-//	_______________Qwerty_Row__2_______________,
-	TD(TD_SFT_CAPS),  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     _______, _______,		KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_T(KC_ENT),
-//	KC_LCTL, KC_LGUI, KC_LALT, GHERKIN, SUBTER, SH_T(KC_SPC),		_______, _______,		SH_T(KC_SPC), SUPRA, KC_RGUI, KC_RALT, GHERKIN, KC_RCTL,
-												_______, _______, _______,		_______, _______, _______
-  ),
-
-  [SUP] = LAYOUT_iris_base_wrapper(
-    ________________SUPRA_Row_0________________,
-    ________________SUPRA_Row_1________________,
-    ________________SUPRA_Row_2________________,
-	________________SUPRA_Row_3________________
-  ),
-  [SUB] = LAYOUT_wrapper(
-    _______________SUBTER_Row__1_______________,
-    _______________SUBTER_Row__0_______________,
-    _______________SUBTER_Row__1_______________,
-    _______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______,  KC_RGUI,ALT_T(KC_DOT),  	CTL_T(KC_BSPC), _______,
-												_______, _______, _______,		KC_ENT,  KC_LSFT, _______
-  ),
-*/
   [NUM] = LAYOUT_wrapper(
     _______________SUBTER_Row__1_______________,
     _______________NUMBERS_Row_0_______________,
     _______________NUMBERS_Row_1_______________,
-    //_______, KC_F11, KC_F12, _______,_______, _______, _______, _______,  KC_ENT, KC_RSFT, KC_RGUI, ALT_T(KC_DOT), CTL_T(KC_BSPC), _______,
     _______, KC_F11, KC_F12, _______,_______, _______,   KC_ENT, KC_RSFT, KC_RGUI, ALT_T(KC_DOT), CTL_T(KC_BSPC), _______,
-												_______, _______, _______,		KC_ENT,  KC_LSFT, _______
+			_______, _______, _______,		KC_ENT,  KC_LSFT, _______
   ),
   [DIR] = LAYOUT_iris_base_wrapper(
     _______________SUBTER_Row__1_______________,
@@ -121,20 +79,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______________SUBTER_Row__1_______________,
     ______________ETCETERA_Row__0______________,
     ______________ETCETERA_Row__1______________,
-    //_______, TD(TD_SFT_CAPS),KC_CAPS, _______,_______, _______, _______, _______,  LALT(LCTL(KC_DEL)),	KC_WH_L, KC_WH_R, KC_LALT, KC_DEL, _______,
-   // _______, KC_LSFT, KC_CAPS, _______,_______, _______, _______, _______,  LALT(LCTL(KC_DEL)),	KC_WH_L, KC_WH_R, KC_LALT, KC_DEL, _______,
    _______, KC_LSFT, KC_CAPS, _______,_______, _______,  LALT(LCTL(KC_DEL)),	KC_WH_L, KC_WH_R, KC_LALT, KC_DEL, _______,
-												_______, _______, _______,		LALT(LCTL(KC_DEL)), _______, _______
+			_______, _______, _______,		LALT(LCTL(KC_DEL)), _______, _______
   )
   
 };
-
-/*
-void matrix_init_keymap(void) {
-  DDRD &= ~(1<<5);
-  PORTD &= ~(1<<5);
-
-  DDRB &= ~(1<<0);
-  PORTB &= ~(1<<0);
-}
-*/
