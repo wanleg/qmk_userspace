d3a429ad2c89c985fc1b3ba09ec38844531ff7f6
wanleg <wanleg@github.com>
2025-11-05 11:53:45 -0800

    move due to compile error

--- layouts/stash/split_3x6_3/wanleg/keymap.c
+++ layouts/stash/split_3x6_3/wanleg/keymap.c
@@ -5,16 +5,19 @@
 //list combos
 enum combo_events {
   PINKY_ENTER,
+  THUMB_POWER,
   //EM_EMAIL,
   COMBO_LENGTH //this is a required line for the COMBO_COUNT delete
 };
 uint16_t COMBO_LEN = COMBO_LENGTH; // remove the COMBO_COUNT define in config.h and use this instead!
 //create keycombo name mappings
 const uint16_t PROGMEM twoKeyEnter[] = {SFT_T(KC_SPC), CTL_T(KC_BSPC), COMBO_END};
+const uint16_t PROGMEM twoThumbPower[] = {ETCETERA, KC_LCTL, COMBO_END};
 //const uint16_t PROGMEM email_combo[] = {KC_G, KC_H, COMBO_END};
 
 combo_t key_combos[] = {
     [PINKY_ENTER] = COMBO_ACTION(twoKeyEnter),
+    [THUMB_POWER] = COMBO_ACTION(twoThumbPower),
     //COMBO(twoKeyEnter, KC_SPC), //can define simple actions here
     //[EM_EMAIL] = COMBO_ACTION(email_combo), //complex actions can be described below
 };
@@ -32,6 +35,12 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
     case PINKY_ENTER:
       if (pressed) {
         tap_code16(KC_ENT);
+      }
+      break;
+
+    case THUMB_POWER:
+      if (pressed) {
+        tap_code16(KC_PWR);
       }
       break;
   }
