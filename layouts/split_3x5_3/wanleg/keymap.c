af827426680cce1a2e08345a1f52b5d5bb025623
wanleg <wanleg@github.com>
2025-11-29 18:39:55 -0800

    thumb enter

--- layouts/split_3x5_3/wanleg/keymap.c
+++ layouts/split_3x5_3/wanleg/keymap.c
@@ -9,8 +9,9 @@
      K01,	K02,	K03,	K04,	K05,	K06,	K07,	K08,	K09,	K0A, \
      K11,	K12,	K13,	K14,	K15,	K16,	K17,	K18,	K19,	K1A, \
      K21,	K22,	K23,	K24,	K25,	K26,	K27,	K28,	K29,	K2A, \
-              KC_LCTL, NUMBER, ETCETERA,	KC_SPC, DIRECTION, KC_RGUI            \
+             KC_LCTL, NUMBER, ETCETERA,	KC_SPC, LT(DIRECTION,KC_ENT), KC_RGUI        \
   )
+
 #define LAYOUT_split_3x5_3_base_wrapper(...)       LAYOUT_split_3x5_3_base(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -18,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______________Gherkin_Row_0_______________, 
   _______________Gherkin_Row_1_______________, 
 //  SFT_T(KC_Z), 	KC_X, 	KC_C, 	LT(NUM,KC_V),	LT(ETC,KC_B),  KC_N, 	LT(DIR,KC_M), 	GUI_T(KC_COMM),	ALT_T(KC_DOT),	CTL_T(KC_BSPC)  
-  SFT_T(KC_Z), 	KC_X, 	KC_C, 	KC_V,	LT(ETC,KC_B),  KC_N, 	KC_M, 	KC_COMM,	ALT_T(KC_DOT),	RCTL_T(KC_BSPC)  
+  SFT_T(KC_Z), 	KC_X, 	KC_C, 	KC_V,	KC_B,  KC_N, 	KC_M, 	KC_COMM,	ALT_T(KC_DOT),	RCTL_T(KC_BSPC)  
 //  KC_Z,    		KC_X,   KC_C,   KC_V,  		    KC_B,     	   KC_N,    KC_M,    		KC_COMM, 		KC_DOT,  		KC_SLSH
 ),
 
--- layouts/split_3x6_3/wanleg/keymap.c
+++ layouts/split_3x6_3/wanleg/keymap.c
@@ -9,7 +9,7 @@
     K00, K01, K02, K03, K04, K05, K06, K07, K08, K09, K0A, K0B, \
     K10, K11, K12, K13, K14, K15, K16, K17, K18, K19, K1A, K1B, \
     K20, K21, K22, K23, K24, K25, K26, K27, K28, K29, K2A, K2B, \
-      KC_LCTL, NUMBER, ETCETERA,	KC_SPC, DIRECTION, KC_RGUI  \
+      KC_LCTL, NUMBER, ETCETERA,	KC_SPC, LT(DIRECTION,KC_ENT), KC_RGUI  \
   )
 #define LAYOUT_split_3x6_3_base_wrapper(...)       LAYOUT_split_3x6_3_base(__VA_ARGS__)
 
